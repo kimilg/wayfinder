f5ee5d12b79798fa0b390e13b3716434a67a56d6
ilgoo.kim <ilgoo.kim@navercorp.com>
2025-06-01 16:44:23 +0900

    tiptap editor styling

--- frontend/src/components/TipTapEditor.jsx
+++ frontend/src/components/TipTapEditor.jsx
@@ -1,9 +1,21 @@
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import Underline from '@tiptap/extension-underline'
+import TextAlign from '@tiptap/extension-text-align'
+import MenuBar from './MenuBar'
+import './TipTapEditor.css';
 
-export default function TipTapEditor({ onAnalyze }) {
+export default function TipTapEditor({onAnalyze}) {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [StarterKit.configure({
+      heading: {levels: [1, 2]},
+      paragraph: true,
+    }),
+      Underline,
+      TextAlign.configure({
+        types: ['heading', 'paragraph'],
+      })
+    ],
     content: '',
   });
 
@@ -16,10 +28,14 @@ export default function TipTapEditor({ onAnalyze }) {
 
   return (
       <div className="p-4 border border-gray-300 rounded bg-white">
-        {editor && 
-            <EditorContent 
+        {editor &&
+            <div className="editor-wrapper">
+              <MenuBar editor={editor}/>
+              <EditorContent 
                 editor={editor} 
-                className="min-h-[150px] bg-yellow-50 p-2 outline-none " />}
+                className="min-h-[150px] bg-yellow-50 p-2 outline-none " />
+            </div>
+        }
         <button
             onClick={handleAnalyze}
             className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
